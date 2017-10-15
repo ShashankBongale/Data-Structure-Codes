3799b2e0092d29eca4076a5eeb6176cc5f31c43b
Shashank_Bongale <shashankbongale@gmail.com>
2017-10-15 11:31:17 +0530

    Array implementation of BST has beeen added

---
+++ tree/arr_imp.c
@@ -0,0 +1,86 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define max 50
+typedef struct node
+{
+int data;
+int info;
+}node;
+int main()
+{
+int data,ch,c;
+node a[max];
+int i;
+int num=0;
+for(i=0;i<max;i++)
+{
+a[i].info=0;
+}
+while(1)
+{
+printf("Enter choice\n");
+printf("1.Insert\n2.Display\n3.Exit\n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1: printf("Enter element\n");
+        scanf("%d",&data);
+        if(a[0].info==0)
+        {
+         a[0].data=data;
+         a[0].info=1;
+        }
+        else
+	{
+	 c=0;
+	 while(c<max && a[c].info!=0)
+	 { if(a[c].data!=data)
+           {
+           if(data>a[c].data)
+            c=2*c+2;
+           else if(data<a[c].data)
+	    c=2*c+1;
+	  }
+         else
+          {
+          printf("Element already present\n");
+          break;
+          }
+         }
+         a[c].info=1;
+         a[c].data=data;
+         num++;
+       }
+       break;
+case 2:for(i=0;i<max;i++)         
+       {
+        if(a[i].info==1)
+        printf("%d ",a[i].data);  /*DISPLAY HERE DOES NOT FOLLOW ANY TRAVERAL METHOD INSTEAD JUST PRINTS
+                                   DIGITS PRESENT IN THE ARRAY IN THE ORDER IN WHICH THEY APPEAR*/      
+       }
+       break;
+case 3:exit(0);
+       break;
+default:printf("Invalid option\n");
+}
+}
+return 0;
+}
+/*
+void iot(node a[])
+{
+c=0;
+iot_r(c);
+}
+void iot(int c)
+{
+static int i=0;
+if(i!=max)
+{
+if(a[i].info!=0)
+ printf("%d ",a[i].data);
+iot(c*2+1);
+iot(c*2+2);
+}
+}
+*/
