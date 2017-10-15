2728f3d217fc8edb0c607c2356acdb988b49afec
Shashank_Bongale <shashankbongale@gmail.com>
2017-10-15 22:16:01 +0530

    Heap sort

---
+++ tree/heap_sort.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+int main()
+{
+int i,a[100],ele;
+int s;
+printf("Enter number of elements\n");
+int n,f;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+ scanf("%d",&a[i]);
+for(i=1;i<n;i++)
+{
+ele=a[i];
+s=i;
+//s=2*f+1;
+f=(s-1)/2;
+while(s>0 && ele>a[f])
+{
+a[s]=a[f];
+s=f;
+f=(s-1)/2;
+}
+a[s]=ele;
+}
+printf("Heapified Elements\n");
+for(i=0;i<n;i++)
+ printf("%d ",a[i]);
+printf("\n");
+//sort
+int e1,e2,temp;
+int j;
+int k;
+for(i=n-1;i>0;i--)
+{
+temp=a[0];
+a[0]=a[i];
+a[i]=temp;
+e2=temp;
+for(j=1;j<i-1;j++)
+{
+ele=a[j];
+s=j;
+f=(s-1)/2;
+while(s>0 && ele>a[f])
+{
+a[s]=a[f];
+s=f;
+f=(s-1)/2;
+}
+a[s]=ele;
+}
+}
+if(n%2==0)
+{
+temp=a[1];
+a[1]=a[2];
+a[2]=temp;
+}
+printf("Sorted elements\n");
+for(i=0;i<n;i++)
+ printf("%d ",a[i]);
+return 0;
+}
